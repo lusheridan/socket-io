f7af4956c455378dcf89e3533f4b7e41bdbb391f
Lucia Sheridan <nluciasheridan@gmail.com>
2022-05-17 18:15:41 -0300

    Motores de Plantillas

--- indexEjs.js
+++ indexEjs.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = require("./routes/index");
-
 const Contenedor = require("./Contenedor");
+
 const contenedor = new Contenedor("./productos.json");
 
 const app = express();
 const PORT = 8080;
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
@@ -16,12 +16,12 @@ app.use("/", express.static(__dirname + "/public"));
 const routerPug = express.Router();
 
 routerPug.get("/productos/create", async (req, res) => {
-  res.render(`${__dirname}/views/pug/formularioProductos.pug`);
+  res.render(`${__dirname}/views/ejs/formularioProductos.ejs`);
 });
 
 routerPug.get("/productos", async (req, res) => {
   const listaProductos = await contenedor.getAll();
-  res.render(`${__dirname}/views/pug/listaProductos.pug`, { listaProductos });
+  res.render(`${__dirname}/views/ejs/listaProductos.ejs`, { listaProductos });
 });
 
 routerPug.post("/productos/create", async (req, res) => {
--- indexHbs.js
+++ indexHbs.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = require("./routes/index");
-
 const Contenedor = require("./Contenedor");
+const { engine } = require("express-handlebars");
+
 const contenedor = new Contenedor("./productos.json");
 
 const app = express();
 const PORT = 8080;
 
-app.set("view engine", "pug");
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", `${__dirname}/views/hbs`);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
@@ -16,12 +19,14 @@ app.use("/", express.static(__dirname + "/public"));
 const routerPug = express.Router();
 
 routerPug.get("/productos/create", async (req, res) => {
-  res.render(`${__dirname}/views/pug/formularioProductos.pug`);
+  res.render(`${__dirname}/views/hbs/formularioProductos.hbs`);
 });
 
 routerPug.get("/productos", async (req, res) => {
   const listaProductos = await contenedor.getAll();
-  res.render(`${__dirname}/views/pug/listaProductos.pug`, { listaProductos });
+  res.render(`${__dirname}/views/hbs/listaProductos.hbs`, {
+    listaProductos,
+  });
 });
 
 routerPug.post("/productos/create", async (req, res) => {
