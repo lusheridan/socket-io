3f46663d7406f9c6598f989dba103c47a3ca96cb
Lucia Sheridan <nluciasheridan@gmail.com>
2022-08-03 21:20:33 -0300

    Gzip y Logger

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 const os = require("os");
 const cluster = require("cluster");
+const compression = require("compression");
+const log4js = require("log4js");
 
 const argv = yargs(hideBin(process.argv)).argv;
 const PORT = 8080;
@@ -35,8 +37,29 @@ if (isCluster && cluster.isMaster) {
 
   dotenv.config();
 
+  log4js.configure({
+    appenders: {
+      logsConsole: { type: "console" },
+      logsWarn: { type: "file", filename: "warn.log" },
+      logsError: { type: "file", filename: "error.log" },
+    },
+    categories: {
+      default: { appenders: ["logsConsole"], level: "all" },
+      console: { appenders: ["logsConsole"], level: "info" },
+      warn: { appenders: ["logsWarn"], level: "warn" },
+      error: { appenders: ["logsError"], level: "error" },
+    },
+  });
+
+  const logConsole = log4js.getLogger("console");
+  const logWarn = log4js.getLogger("warn");
+  const logError = log4js.getLogger("error");
+
   httpServer.listen(currentPort, () => {
     console.log(`Server on port ${currentPort}`);
+    logConsole.info("Info");
+    logWarn.warn("Warn");
+    logError.error("Error");
   });
 
   function isAuth(req, res, next) {
@@ -103,7 +126,7 @@ if (isCluster && cluster.isMaster) {
     });
   });
 
-  app.get("/info", (req, res) => {
+  app.get("/info", compression(), (req, res) => {
     res.send({
       sistemaOperativo: process.platform,
       versionNode: process.version,
